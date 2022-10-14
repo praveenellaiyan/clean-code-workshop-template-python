4907eda27e2a114fb0d692bfd1489c481f993bd3
praveen.ellaiyan <praveen.ellaiyan@thoughtworks.com>
2022-10-14 18:00:45 +0530

    [Rafiq|Praveen] Add html formatter support for statement generation

--- src/movie_rental/Customer.py
+++ src/movie_rental/Customer.py
@@ -1,3 +1,5 @@
+from typing import Callable
+
 from src.movie_rental.Movie import Movie
 from src.movie_rental.Rental import Rental
 
@@ -17,35 +19,60 @@ class Customer:
         return self.name
 
     def statement(self) -> str:
-        total_amount: float = 0.0
-        frequent_renter_points: int = 0
         result: str = "Rental Record for " + self.get_name() + "\n"
+        # show figure for this rental
+        result += self.construct_statement(
+            lambda rental: "\t" + rental.get_movie().get_title() + "\t" + str(self.__rental_amount(rental)) + "\n"
+        )
+        # add footer lines result
+        result += "Amount owed is " + str(self.__total_amount()) + "\n"
+        result += "You earned " + str(self.__frequent_renter_points()) + \
+                  " frequent renter points"
+        return result
+
+    def __total_amount(self):
+        total_amount: float = 0.0
         for each in self.rentals:
-            this_amount: float = 0
-            # determine amounts for each line
-            match each.get_movie().get_price_code():
-                case Movie.REGULAR:
-                    this_amount += 2
-                    if each.get_days_rented() > 2:
-                        this_amount += (each.get_days_rented() - 2) * 1.5
-                case Movie.NEW_RELEASE:
-                    this_amount += each.get_days_rented() * 3
-                case Movie.CHILDRENS:
-                    this_amount += 2
-                    if each.get_days_rented() > 3:
-                        this_amount += (each.get_days_rented() - 3) * 1.5
+            total_amount += self.__rental_amount(each)
+        return total_amount
+
+    def __rental_amount(self, rental):
+        this_amount: float = 0
+        # determine amounts for each line
+        if rental.get_movie().get_price_code() == Movie.REGULAR:
+            this_amount += 2
+            if rental.get_days_rented() > 2:
+                this_amount += (rental.get_days_rented() - 2) * 1.5
+        elif rental.get_movie().get_price_code() == Movie.NEW_RELEASE:
+            this_amount += rental.get_days_rented() * 3
+        elif rental.get_movie().get_price_code() == Movie.CHILDRENS:
+            this_amount += 2
+            if rental.get_days_rented() > 3:
+                this_amount += (rental.get_days_rented() - 3) * 1.5
+        return this_amount
+
+    def __frequent_renter_points(self):
+        frequent_renter_points: int = 0
+        for each in self.rentals:
             # add frequent renter points
             frequent_renter_points += 1
             # add bonus for a two day new release rental
             if (each.get_movie().get_price_code() == Movie.NEW_RELEASE) and \
                     (each.get_days_rented() > 1):
                 frequent_renter_points += 1
-            # show figure for this rental
-            result += "\t" + each.get_movie().get_title() + "\t" + \
-                      str(this_amount) + "\n"
-            total_amount += this_amount
-        # add footer lines result
-        result += "Amount owed is " + str(total_amount) + "\n"
-        result += "You earned " + str(frequent_renter_points) + \
-                  " frequent renter points"
-        return result
+        return frequent_renter_points
+
+    def construct_statement(self, formatter: Callable):
+        formatted_statement = ""
+        for rental in self.rentals:
+            formatted_statement += formatter(rental)
+        return formatted_statement
+
+    def html_statement(self):
+        return "<html><h1>Rental Record for <b>"+self.name+"</b></h1></br>" \
+                + self.construct_statement(
+                    lambda rental: " "+rental.movie.get_title()+" "+str(self.__rental_amount(rental))+"</br>"
+                  ) + \
+               "Amount owed is <b>"+str(self.__total_amount())+"</b></br>" \
+               "You earned <b>"+str(self.__frequent_renter_points())+"</b> frequent renter points</html>"
+
--- test/test_Customer.py
+++ test/test_Customer.py
@@ -1,2 +1,31 @@
+from src.movie_rental.Customer import Customer, Movie, Rental
+
+
 def test_statement():
-    assert False
+    assert True
+
+def test_characterisation():
+    customer: Customer = Customer("BoB")
+    customer.add_rental(Rental(Movie("movie-1", Movie.REGULAR), 10))
+    customer.add_rental(Rental(Movie("movie-2", Movie.CHILDRENS), 10))
+    customer.add_rental(Rental(Movie("movie-3", Movie.NEW_RELEASE), 10))
+
+    assert "Rental Record for BoB\n" \
+           "\tmovie-1\t14.0\n" \
+           "\tmovie-2\t12.5\n" \
+           "\tmovie-3\t30\n" \
+           "Amount owed is 56.5\n" \
+           "You earned 4 frequent renter points" == customer.statement()
+def test_html_statement():
+    customer: Customer = Customer("BoB")
+    customer.add_rental(Rental(Movie("movie-1", Movie.REGULAR), 10))
+    customer.add_rental(Rental(Movie("movie-2", Movie.CHILDRENS), 10))
+    customer.add_rental(Rental(Movie("movie-3", Movie.NEW_RELEASE), 10))
+
+    assert "<html><h1>Rental Record for <b>BoB</b></h1></br>" \
+           " movie-1 14.0</br>" \
+           " movie-2 12.5</br>" \
+           " movie-3 30</br>" \
+           "Amount owed is <b>56.5</b></br>" \
+           "You earned <b>4</b> frequent renter points</html>" == customer.html_statement()
+
